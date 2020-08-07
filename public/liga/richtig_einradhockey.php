9e506d53a16d7310a34895b35f4e28e6303214ba
Ansgar <merkmich@gmail.com>
2020-08-07 12:50:19 +0200

    Init

---
+++ public/liga/richtig_einradhockey.php
@@ -0,0 +1,142 @@
+<?php
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LOGIK////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+require_once '../../logic/first.logic.php'; //autoloader und Session
+
+/////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////LAYOUT///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+$titel = "Richtig Einradhockey | Deutsche Einradhockeyliga";
+$content= "Dieser Guide soll ein Anhaltspunkt für Einradhockeyeinsteiger und -fortgeschrittene sein, wie man richtig Einradhockey spielen kann.";
+include '../../templates/header.tmp.php';
+?>
+<h1 class="w3-text-primary w3-center w3-bottombar w3-border-tertiary">Richtig Einradhockey</h1>
+<p class="w3-text-grey">Diese Seite soll ein Anhaltspunkt für Einradhockeyeinsteiger und -fortgeschrittene sein, wie man richtig Einradhockey spielen kann. Hierbei ist natürlich zu beachten, dass dieser Guide nicht den Anspruch hat immer perfekt zu sein. Er fußt hauptsächlich auf Erfahrungen des in Mörfelden gespielten Einradhockeys (17x&nbsp;Deutscher Meister seit 2000, Stand 2020).</p>
+<h2 class="w3-text-primary">Allgemeines</h2>
+<p class="">Einradfahrer sollten erst mit dem Einradhockey beginnen, wenn sie sicher Einradfahren können. Sie sollten also mindestens Vorwärts, Rückwärts, den freien Aufstieg und das enge Kurvenfahren beherrschen. Einbeinigfahren ist ein guter Anhaltspunkt, dass ein Einradfahrer fürs Einradhockey bereit ist.</p>
+<h2 class="w3-text-primary">Ausrüstung</h2>
+<div class="w3-responsive w3-card ">
+    <table class="w3-table w3-striped">
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Einrad</th>
+            <td>Fürs Einradhockey sind 20&nbsp;Zoll Freestyle-Einräder zu empfehlen. Das Einrad sollte keine losen Teile haben und in einem guten Zustand sein. Je leichter das Einrad, desto besser. Für den Torwart ist ein 24&nbsp;Zoll Rad empfehlenswert.</td>
+        </tr>
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Reifen</th>
+            <td>Reifenbreite: 2,1&nbsp;Zoll oder 1,95&nbsp;Zoll. Im A-Bereich sind Reifen mit 2,1&nbsp;Zoll beliebt. Reifen sollten nicht schwarz sein. Spieler sollten sich regelmäßig neue Reifen zulegen, insbesondere wenn der alte Reifen an Profilierung verliert oder das Gummi aushärtet.</td>
+        </tr>
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Felgen</th>
+            <td>Normale Freestyle-Felgen sind völlig ausreichend. Breite (Trial-) Felgen sollten vermieden werden.</td>
+        </tr>
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Kurbeln</th>
+            <td>Kurbellänge 100&nbsp;cm oder 115&nbsp;cm, im Idealfall aus Aluminium</td>
+        </tr>
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Pedale</th>
+            <td>Pedale aus Kunststoff mit viel Grip</td>
+        </tr>
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Schuhe</th>
+            <td>Hallenschuhe mit dünner Sohle und kurzen Schnürsenkeln</td>
+        </tr>
+        <tr>
+            <th class="w3-primary" style="vertical-align: middle;">Schläger</th>
+            <td>Schläger sollten leicht sein. One-Piece Schläger sind gegenüber Schlägern mit gesteckter Kelle zu empfehlen. Wenn die Kelle geschäumt ist, kann es sein, dass sich die Kelle nach einiger Zeit spaltet. Bei hochwertigen Schlägern (>&nbsp;100&nbsp;Euro) tritt dies später auf, als bei günstigen. Holzschläger sind nicht mehr zu empfehlen. Damit die Kelle nicht abgerieben wird, kann man die Kelle am unteren Ende regelmäßig mit richtigem Eishockeytape tapen.</td>
+        </tr>
+    </table>
+</div>
+<h2 class="w3-text-primary">Haltung</h2>
+<h3 class="w3-text-grey">Sitzhöhe</h3>
+<div class="w3-margin-left">
+    <ul class="w3-ul w3-leftbar w3-border-tertiary">
+        <li>Beine in der weitesten Pedalstellung leicht angewinkelt</li>
+        <li>Sitzhöhe ist tendenziell eher niedriger als höher</li>
+    </ul>
+</div>
+<h3 class="w3-text-grey">Schlägerlänge</h3>
+<div class="w3-margin-left">
+    <ul class="w3-ul w3-leftbar w3-border-tertiary">
+        <li>Auf dem Einrad sitzend: Schläger reicht vom Boden aus etwa zwischen Brustbein und Kinn</li>
+        <li>Guter Ansatzpunkt: leicht unter Schulterhöhe</li>
+    </ul>
+</div>
+<h3 class="w3-text-grey">Schlägerhaltung</h3>
+<div class="w3-margin-left">
+    <ul class="w3-ul w3-leftbar w3-border-tertiary">
+        <li>Schlägerseite ist unabhängig von Rechts- oder Linkshändigkeit</li>
+        <li>Im aktiven Spiel: Nicht auf den Schläger stützen - beide Daumen müssen am Schläger immer nach unten zeigen</li>
+        <li>Obere Hand deckt das Schlägerende ab, die untere Hand ist flexibel am Schläger</li>
+        <li>Handgelenke sollten beim Spielen aktiv benutzt werden</li>
+    </ul>
+</div>
+<h3 class="w3-text-grey">Richtige Fußstellung</h3>
+<p>Die Füße sollten auf den Pedalen nicht zu weit außen stehen, oder sogar nach außen hin abknicken. Mit den vorderen Fußballen auf den Pedalen stehen.</p>
+<h2 class="w3-text-primary">Technik</h2>
+<h3 class="w3-text-grey">Ballführen</h3>
+<p>In der Regel sollte man den Ball grundsätzlich vor sich führen, nicht neben sich. Abwechselnd Vor- und Rückhandberührungen beim Ballführen.</p>
+<h3 class="w3-text-grey">Pässe</h3>
+<div class="w3-margin-left">
+    <ul class="w3-ul w3-leftbar w3-border-tertiary w3-primary">
+    <li>Pässe sollten unbedingt immer flach gespielt werden: Flache Pässe kann man leicht spielen, indem man beim Passen den Schläger mit Ball in der Kelle aktiv über den Boden zieht (man sollte ihn hören können).</li>
+        <li>Pässe in den Fahrweg des passanehmenden Spielers spielen</li>
+        <li>Man sollte sowohl Rückhand- wie auch Vorhandpässe verwenden</li>
+        <li>Hochschauen zum Spieler, dem man zupasst</li>
+        <li>Bälle bleiben bei der Ballannahme flach, wenn man diese mit Gefühl annimmt, die Kelle nicht zu offen hält und den Schläger nicht zu fest hält.</li>
+        <li>Spieler sollten ihre Kelle dahin halten, wo sie den Pass auch bekommen wollen.</li>
+    </ul>
+</div>
+<h3 class="w3-text-grey">Schlenzen</h3>
+<p>Beim Schlenzen den Oberkörper über den Schläger beugen und mit dem Schläger Druck auf den Boden ausüben. Handgelenke beim Schlenzen mitbenutzen.</p>
+<h3 class="w3-text-grey">Schießen</h3>
+<p>Mit viel Kraft kurz ausgeholt, bei Linksauslegern beim Ausholen die rechte Schulter vorziehen (linke Schulter bei Rechtsauslegern). Auf den richtigen Treffpunkt des Balles achten (erkennt man am Geräusch). Beim Schießen sollte man "Holzfäller-Schüsse" vermeiden: Also Schüsse mit einer sehr weiten schwungvollen Ausholbewegung und Verdrehung des Einrades.</p>
+<h2 class="w3-text-primary">Boxsystem</h2>
+<p>Das Boxsystem hat sich inzwischen etabliert und wird von einigen guten Teams gespielt. Es ist wichtig, dass jeder Spieler aufmerksam spielt und sich bewusst ist, wo und wie seine Mitspieler fahren.</p>
+<h3 class="w3-text-grey">Grundaufstellung</h3>
+<div style="max-width: 700px">
+    <a href='../bilder/richtig_einradhockey/grundaufstellung.png'>
+        <img class="w3-image" src="../bilder/richtig_einradhockey/grundaufstellung.png">
+    </a>
+</div>
+<p>Es gibt zwei Verteidiger und zwei Stürmer. Positionen sollten hierbei nicht zu starr eingehalten werden. Verteidiger können zum Beispiel mit Stürmern tauschen und in den Angriff fahren, der Stürmer muss dann dementsprechend die Position des Verteidigers übernehmen. Der Torwart wird aktiv ins Spiel eingebunden und sollte sich immer anbieten. Stürmer müssen in der Verteidigung zurücksprinten und aktiv aushelfen.</p>
+<h3 class="w3-text-grey">Angriff</h3>
+<p>Im Angriff sollten die Stürmer versetzt stehen und sich immer anbieten und aktiv in Bewegung bleiben. Wichtig ist, dass die Stürmer dabei nicht immer außen kleben bleiben, sondern zumindest ein Stürmer in die Mitte zieht. Die Verteidiger rücken bis zur Mittellinie auf.</p>
+<div style="max-width: 700px">
+    <a href='../bilder/richtig_einradhockey/angriff.png'>
+        <img class="w3-image" src="../bilder/richtig_einradhockey/angriff.png">
+    </a>
+</div>
+<h3 class="w3-text-grey">Spielverlagerung</h3>
+<p>Je nach Ballposition verlagert sich das Spiel nach links und rechts. Im echten Spiel muss man natürlich das Spiel "lesen" und dementsprechend fahren. Im Grundsatz gilt aber:</p>
+<div class="w3-row-padding w3-stretch">
+    <div class="w3-col s12 m6">
+        <a href='../bilder/richtig_einradhockey/links.png'>
+            <img class="w3-image" src="../bilder/richtig_einradhockey/links.png">
+        </a>
+        <p>Befindet sich der Ball beim linken Stürmer, rückt der rechte Verteidiger bis zum Mittelpunkt auf. Der linke Verteidiger bietet sich an der Linie an. Der rechte Stürmer bietet sich versetzt zum linken Stürmer an.</p>
+    </div>
+    <div class="w3-col s12 m6">
+        <a href='../bilder/richtig_einradhockey/rechts.png'>
+            <img class="w3-image" src="../bilder/richtig_einradhockey/rechts.png">
+        </a>
+        <p>Befindet sich der Ball beim rechten Stürmer, rückt der linke Verteidiger bis zum Mittelpunkt auf. Der rechte Verteidiger bietet sich an der Linie an. Der linke Stürmer bietet sich versetzt zum rechten Stürmer an.</p>
+    </div>
+</div>
+<h3 class="w3-text-grey">Abstoss</h3>
+<p>Hat der Gegner nach einem Tor den Ball, ist es wichtig die Mitte zuzumachen. Die Spieler bilden jetzt ein "T", dabei rückt einer der Verteidiger nach außen an die Mittellinie vor und der andere Verteidiger die Mitte. ein Stürmer übernimmt den Mittelpunkt.</p>
+<div style="max-width: 700px">
+    <a href='../bilder/richtig_einradhockey/abstoss.png'>
+        <img class="w3-image" src="../bilder/richtig_einradhockey/abstoss.png">
+    </a>
+</div>
+<h3 class="w3-text-grey">Bully</h3>
+<p>Dies gilt ebenfalls für den Bully zu Spielbeginn.</p>
+<div style="max-width: 700px">
+    <a href='../bilder/richtig_einradhockey/bully.png'>
+        <img class="w3-image" src="../bilder/richtig_einradhockey/bully.png">
+    </a>
+</div>
+
+<?php include '../../templates/footer.tmp.php';
