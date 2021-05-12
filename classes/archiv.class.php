c8c430657a9b459b3f5c5a0eb17ac6bc46b6bcc5
Philipp Gross <pka.gross@web.de>
2021-05-12 20:10:47 +0200

    Turniertransfer hinzugefügt

--- classes/archiv.class.php
+++ classes/archiv.class.php
@@ -6,8 +6,7 @@ class Archiv
      * Übertragen der Teams in die Archivdatenbank.
      * @param $saison
      */
-    
-    public static function transfer_teams($saison)
+    public static function transfer_teams(int $saison)
     {
         $extract_sql = "
             SELECT team_id, teamname, ligateam, ? as saison
@@ -32,5 +31,37 @@ class Archiv
 
         db::terminate();
         db::initialize();
+    }
+
+    /**
+     * Übertragen der Turniere in die Archivdatenbank.
+     * @param $saison
+     */
+    public static function transfer_turniere(int $saison) {
+        $extract_sql = "
+            SELECT turnier_id, tname, art, ausrichter, tblock, datum, saison 
+            FROM `turniere_liga` 
+            WHERE saison = ? 
+            AND turniere_liga.phase = 'ergebnis'
+        ";
+
+        $result = db::$db->query($extract_sql, $saison)->esc()->fetch();
+
+        db::terminate();
+        
+        db::initialize(Env::HOST_NAME, Env::USER_NAME, Env::PASSWORD, 'db_einradhockey_archiv');
+
+        $insert_sql = "
+            INSERT INTO turniere_liga (turnier_id, tname, art, ausrichter, tblock, datum, saison)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        ";        
+
+        foreach ($result as $turnier) {
+            db::$db->query($insert_sql, $turnier['turnier_id'], $turnier['tname'], $turnier['art'], $turnier['ausrichter'], $turnier['tblock'], $turnier['datum'], $turnier['saison']);
+        }
+
+        db::terminate();
+        db::initialize();
+
     }
 }
