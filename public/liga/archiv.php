3451a3da3aa7da12b91b2951d99a68da10e61995
Philipp Gross <pka.gross@web.de>
2021-05-13 00:11:02 +0200

    Archivseite weiter vorbereitet

--- public/liga/archiv.php
+++ public/liga/archiv.php
@@ -16,7 +16,8 @@ include '../../templates/header.tmp.php';
 
 <!-- Archiv -->
 <h1 class="w3-text-primary">Archiv</h1>
-<!-- iframes sind ein sonderfall, html5 depreciated -->
+
+<p>Die Deutsche Einradhockeyliga spielt seit 1995 den <i>Meister der Deutschen Einradhockeyliga</i> aus. In der ersten Saison wurde nach einem Modus Jeder-gegen-Jeden gespielt. Meister wurde das Team mit den meisten Punkten. Seit 1996 wird in einem Turniermodus gespielt und am Ende der Saison in einem Finalturnier der Meister der Deutschen Einradhockeyliga ermittelt.</p>
 
 <table class="w3-table w3-striped">
     <thead class="w3-primary">
@@ -29,7 +30,7 @@ include '../../templates/header.tmp.php';
     </thead>
 <?php foreach ($uebersicht as $saison) {?>
     <tr>
-        <td><?=$saison['saison'] + 1995?></td>
+        <td><?=$saison['saison']?></td>
         <td><?=$saison['turnier_anzahl']?></td>
         <td><?=$saison['teams_anzahl']?></td>
         <td><?=$saison['meister'] ?? 'Kein Meister ermittelt'?></td>
@@ -37,6 +38,7 @@ include '../../templates/header.tmp.php';
 <?php } ?>
 </table>
 
-<iframe src="<?= Nav::LINK_ARCHIV ?>" style="width:100%;height:800px;" class="archiv w3-border-0" title="Archiv der Deutschen Einradhockeyliga"></iframe>
+<!-- iframes sind ein sonderfall, html5 depreciated -->
+<!-- <iframe src="<?= Nav::LINK_ARCHIV ?>" style="width:100%;height:800px;" class="archiv w3-border-0" title="Archiv der Deutschen Einradhockeyliga"></iframe> -->
 
 <?php include '../../templates/footer.tmp.php';
