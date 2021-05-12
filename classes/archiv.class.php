8586073581a1f719896081c2b544eaed154877e9
Philipp Gross <pka.gross@web.de>
2021-05-12 15:54:53 +0200

    Teamtransfer hinzugefügt

--- classes/archiv.class.php
+++ classes/archiv.class.php
@@ -1,6 +1,36 @@
 <?php
 
-class Archiv 
+class Archiv
 {
+    /**
+     * Übertragen der Teams in die Archivdatenbank.
+     * @param $saison
+     */
+    
+    public static function transfer_teams($saison)
+    {
+        $extract_sql = "
+            SELECT team_id, teamname, ligateam, ? as saison
+            FROM `teams_liga`
+            WHERE aktiv = 'Ja'
+        ";
 
+        $result = db::$db->query($extract_sql, $saison)->esc()->fetch();
+
+        db::terminate();
+        
+        db::initialize(Env::HOST_NAME, Env::USER_NAME, Env::PASSWORD, 'db_einradhockey_archiv');
+
+        $insert_sql = "
+            INSERT INTO teams (team_id, teamname, ligateam, saison)
+            VALUES (?, ?, ?, ?)
+        ";        
+
+        foreach ($result as $team) {
+            db::$db->query($insert_sql, $team['team_id'], $team['teamname'], $team['ligateam'], $team['saison']);
+        }
+
+        db::terminate();
+        db::initialize();
+    }
 }
