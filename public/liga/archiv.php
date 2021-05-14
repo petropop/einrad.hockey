f11fdac85371e25858aa42f8f7126057fe2c11b6
Philipp Gross <pka.gross@web.de>
2021-05-14 12:59:11 +0200

    Übergabe und Seite hinzugefügt

--- public/liga/archiv.php
+++ public/liga/archiv.php
@@ -19,24 +19,26 @@ include '../../templates/header.tmp.php';
 
 <p>Die Deutsche Einradhockeyliga spielt seit 1995 den <i>Meister der Deutschen Einradhockeyliga</i> aus. In der ersten Saison wurde nach einem Modus Jeder-gegen-Jeden gespielt. Meister wurde das Team mit den meisten Punkten. Seit 1996 wird in einem Turniermodus gespielt und am Ende der Saison in einem Finalturnier der Meister der Deutschen Einradhockeyliga ermittelt.</p>
 
-<table class="w3-table w3-striped">
-    <thead class="w3-primary">
+<div class="w3-responsive w3-card">
+    <table class="w3-table w3-striped">
+        <thead class="w3-primary">
+            <tr>
+                <th><b>Saison</b></th>
+                <th><b>Turniere</b></th>
+                <th><b>Teams</b></th>
+                <th><b>Meister der Deutschen Einradhockeyliga</b></th>
+            </tr>
+        </thead>
+    <?php foreach ($uebersicht as $saison) {?>
         <tr>
-            <th><b>Saison</b></th>
-            <th><b>Turniere</b></th>
-            <th><b>Teams</b></th>
-            <th><b>Meister der Deutschen Einradhockeyliga</b></th>
+            <td><?=Html::link('archiv_saison.php?saison='. $saison['saison'], $saison['saison'], false)?></a></td>
+            <td><?=$saison['turnier_anzahl']?></td>
+            <td><?=$saison['teams_anzahl']?></td>
+            <td><?=$saison['meister'] ?? 'Kein Meister ermittelt'?></td>
         </tr>
-    </thead>
-<?php foreach ($uebersicht as $saison) {?>
-    <tr>
-        <td><?=$saison['saison']?></td>
-        <td><?=$saison['turnier_anzahl']?></td>
-        <td><?=$saison['teams_anzahl']?></td>
-        <td><?=$saison['meister'] ?? 'Kein Meister ermittelt'?></td>
-    </tr>
-<?php } ?>
-</table>
+    <?php } ?>
+    </table>
+</div>
 
 <!-- iframes sind ein sonderfall, html5 depreciated -->
 <!-- <iframe src="<?= Nav::LINK_ARCHIV ?>" style="width:100%;height:800px;" class="archiv w3-border-0" title="Archiv der Deutschen Einradhockeyliga"></iframe> -->
